dbdfe08822bd03e7bfd21afc6d42ab8435e08d75
Marawanlasheen <marawanyasserlasheen@gmail.com>
2025-04-29 22:26:07 +0300

    minor changes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -12,9 +12,6 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
